d28338e494e5e5fd8302f3ebb16d776af9a9e30d
Jakub Rosiński <87801525+BlanKu@users.noreply.github.com>
2024-06-19 13:29:38 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Kolokwum_Java
+ Pomoce naukowe na potrzeby kolokiwum z Javy
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        MultiWatek watek = new MultiWatek(5,10);
+
+        watek.start();
+        watek.start();
+    }
+}
